eb5cedb16d834a340cd2bd2182615496775e1b44
Uzay Altıner <92029942+uzayaltiner@users.noreply.github.com>
2023-03-29 01:02:14 +0300

    tests functions created

--- Tests/ImagePickerViewTests/ImagePickerViewTests.swift
+++ Tests/ImagePickerViewTests/ImagePickerViewTests.swift
@@ -1,11 +1,34 @@
-import XCTest
 @testable import ImagePickerView
+import XCTest
 
 final class ImagePickerViewTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    var imagePickerView: ImagePickerView!
     
+    override func setUp() {
+        super.setUp()
+        imagePickerView = ImagePickerView()
     }
+    
+    override func tearDown() {
+        imagePickerView = nil
+        super.tearDown()
+    }
+    
+    func testPlaceholderImage() {
+        let placeholderImage = UIImage(named: "test_image")
+        imagePickerView.placeholderImage = placeholderImage
+        XCTAssertEqual(imagePickerView.image, placeholderImage)
+    }
+    
+    func testColor() {
+        let testColor = UIColor.red
+        imagePickerView.color = testColor
+        XCTAssertEqual(imagePickerView.backgroundColor, testColor)
+    }
+    
+    func testCornerRadius() {
+           let testCornerRadius: CGFloat = 10.0
+           imagePickerView.cornerRadius = testCornerRadius
+           XCTAssertEqual(imagePickerView.layer.cornerRadius, testCornerRadius)
+       }
 }
